951287dbbffe1f3f4096cca10d1ca7dc23551886
jafeeza <71497464+jafeeza@users.noreply.github.com>
2025-06-30 01:01:58 -0500

    Time: 10 ms (34.94%) | Memory: 21.1 MB (43.24%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        m=[]
+        intervals.sort(key=lambda x:x[0])
+        for i in intervals:
+            if not m or m[-1][1]<i[0]:
+                m.append(i)
+            else:
+                m[-1][1]=max(m[-1][1],i[1])
+        return m
