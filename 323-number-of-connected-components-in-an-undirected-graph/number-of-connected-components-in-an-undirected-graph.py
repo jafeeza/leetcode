35e40e77f5a7c798303e104f9c1032102caea34b
jafeeza <71497464+jafeeza@users.noreply.github.com>
2025-07-12 00:49:02 -0500

    Time: 2 ms (94.23%) | Memory: 18.9 MB (74.84%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,27 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        #Using UNION FIND ( UNION DISJOIN SETS)   
-        parent = [i for i in range(n)]
-        rank = [1]*n #number of nodes
-        
-        #FIND
-        def find(n1): #find root parent
-            res = n1
-            while res!= parent[res]: #recursively find parent 
-                parent[res] = parent[parent[res]]
-                res = parent[res]
+        par=[i for i in range(n)]
+        rank=[1]*n
+
+        def find(n1):
+            res=n1
+            while res!=par[res]:
+                par[res]=par[par[res]]
+                res=par[res]
             return res
-        #UNION
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            if p1 == p2:
+
+        def union(n1,n2):
+            p1,p2=find(n1),find(n2)
+            if p1==p2:
                 return 0
-            if rank[p2] > rank[p1]:
-                parent[p1] = p2
-                rank[p2] += rank[p1]
+            if rank[p2]>rank[p1]:
+                par[p1]=p2
+                rank[p2]+=rank[p1]
             else:
-                parent[p2] = p1
-                rank[p1] += rank[p2]
+                par[p2]=p1
+                rank[p1]+=rank[p2]
             return 1
-        
-        #Go through edges
-        res = n
-        for n1, n2 in edges:
-            res -= union(n1, n2)
+        res=n
+        for n1,n2 in edges:
+            res-=union(n1,n2)
         return res
