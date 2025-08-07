cd2a015ea3c6418d8c824c9039e4454354f00c0f
jafeeza <71497464+jafeeza@users.noreply.github.com>
2025-08-07 02:36:25 -0500

    Time: 1046 ms (52.63%) | Memory: 133.2 MB (21.05%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.py
@@ -0,0 +1,52 @@
+class Solution:
+    def maxCollectedFruits(self, mat: List[List[int]]) -> int:
+        self.n=len(mat)
+        memo=[[-1]*self.n for _ in range(self.n)]
+        def dfs2(row,col):
+            if row < 0 or col < 0 or row >= self.n or col >= self.n:
+                return 0
+            if memo[row][col] != -1:
+                return memo[row][col]
+            val = mat[row][col]
+            res = 0
+            if row == col:
+                res = max(res, dfs2(row + 1, col + 1))
+            elif row - 1 == col:
+                res = max(res, dfs2(row + 1, col + 1),dfs2(row,col+1))
+            else:
+                res = max(res, dfs2(row + 1, col + 1),dfs2(row,col+1),dfs2(row-1,col+1))
+            memo[row][col]=val+res
+            return memo[row][col]
+
+        def dfs3(row,col):
+            if row < 0 or col < 0 or row >= self.n or col >= self.n:
+                return 0
+            if memo[row][col] != -1:
+                return memo[row][col]
+            val = mat[row][col]
+            res = 0
+
+            if row == col:
+                res = max(res, dfs3(row + 1, col + 1))
+            elif row == col - 1:
+                res = max(res, dfs3(row + 1, col + 1),dfs3(row+1,col))
+            else:
+                res = max(res, dfs3(row + 1, col + 1),dfs3(row+1,col),dfs3(row+1,col-1))
+            memo[row][col]=val+res
+            return memo[row][col]
+            
+
+        total = 0
+
+        # child - 1
+        # he will eat all diagonal fruits, so set them to 0 
+        for i in range(self.n):
+            total += mat[i][i]
+            mat[i][i] = 0
+
+        # child - 2
+        total += dfs2(self.n - 1, 0)
+        # child - 3
+        total += dfs3(0, self.n - 1)
+
+        return total
