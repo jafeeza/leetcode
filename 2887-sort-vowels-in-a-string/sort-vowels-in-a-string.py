a3f54d3df4fd6328177013dfbb97adc96291d1ab
jafeeza <71497464+jafeeza@users.noreply.github.com>
2025-09-11 17:59:47 -0500

    Time: 75 ms (75.58%) | Memory: 20.6 MB (53.47%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,24 +1,16 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
         vowels = []
-        s_list = list(s)
-        # collect all vowels
+        s_list=list(s)
         for i in s_list:
             if i in "AEIOUaeiou":
                 vowels.append(i)
-        
-        if vowels == []:
+        if vowels==[]:
             return s
-
-        # sort the vowels
         vowels.sort()
-
-        count = 0
-
-        # replace original vowels with sorted ones
+        count=0
         for j in range(len(s)):
             if s_list[j] in "AEIOUaeiou":
-                s_list[j] = vowels[count]
-                count += 1
-        
+                s_list[j]=vowels[count]
+                count+=1
         return "".join(s_list)
