aae2052c618e0f4c9a80f8f56a5c07b59ce7f5b0
jafeeza <71497464+jafeeza@users.noreply.github.com>
2025-11-13 17:57:57 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.72%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,10 +6,29 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        U:p and q are two trees I need return true both the trees same, like same values and False if it's not.
+        P:
+        Going for BFS, checking the root value and the children values like 
+        p.left==q.left and p.right==q.right
+        I:
+        EC: both of roots empty, return True
+            one of the tree present and the other return False
+            the checking the values of children by traversing through the function
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val!=q.val:
+                return False
+            return isSameTree(self,p.left,q.left) and isSameTree(self,p.right,q.right)
+        """
         if not p and not q:
             return True
         if not p or not q:
             return False
-        if p.val != q.val:
+        if p.val!=q.val:
             return False
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+        
